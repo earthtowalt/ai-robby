b3c1920e743b32c9dae7a4e4d7025f130eafa42b
earthtowalt <jackwalton00@aol.com>
2021-11-03 17:47:45 -0500

    put The runGen function into the Runner script

--- GARunner.py
+++ GARunner.py
@@ -26,8 +26,32 @@ generation_count = 500
 
 output_file = 'GAoutput_{0}.txt'.format(time.strftime("%H-%M-%S",time.localtime()))
 
-def modified_demo(strategy):
-    return random.randint(0,501)
+def runGen(strategy, steps=200, init=0.50):
+    if type(strategy) is not str or len(strategy) != 243:
+        raise Exception("strategy is not a string of length 243")
+    for char in strategy:
+        if char not in "0123456":
+            raise Exception("strategy contains a bad character: '%s'" % char)
+    if type(steps) is not int or steps < 1:
+        raise Exception("steps must be an integer > 0")
+    if type(init) is str:
+        # init is a config file
+        rw.load(init)
+    elif type(init) in [int, float] and 0 <= init <= 1:
+        # init is a can density
+        rw.goto(0, 0)
+        rw.distributeCans(init)
+    else:
+        raise Exception("invalid initial configuration")
+    history = []
+    cycleDetected = False
+    cycleStart = 0
+    reward = 0
+    for i in xrange(steps):
+        p = rw.getPerceptCode()
+        action = robby.POSSIBLE_ACTIONS[int(strategy[p])]
+        reward += rw.performAction(action)
+    return reward
 
 
 
@@ -36,7 +60,7 @@ for g in xrange(generation_count):
     results = []
     # loop over population
     for strategy in population: 
-        results.append((modified_demo(strategy), strategy))
+        results.append((runGen(strategy), strategy))
         # -- modified demo() -- Shepard
         # create reward counter
         # loop over #actions
