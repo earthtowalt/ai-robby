d3e12f24374e79dc9a869f67511b124ac61ffa3f
earthtowalt <jackwalton00@aol.com>
2021-11-03 21:19:55 -0500

    kinda working

--- GARunner.py
+++ GARunner.py
@@ -1,7 +1,10 @@
 import random
 import time
+import math
 
 import robby 
+import numpy as np
+
 rw = robby.World(10,10)
 
 # rw.demo(rw.strategyM)
@@ -20,13 +23,18 @@ rw = robby.World(10,10)
 def generateRandomStrategy():
     return ''.join([str(random.choice(list(xrange(7)))) for i in xrange(243)])
 
-population = [generateRandomStrategy() for i in xrange(200)]
+POP_SIZE = 200
+population = [generateRandomStrategy() for i in xrange(POP_SIZE)]
 
-generation_count = 500
+GENERATIONS = 1000
+STEPS = 200
+CANS = 0.5
 
 output_file = 'GAoutput_{0}.txt'.format(time.strftime("%H-%M-%S",time.localtime()))
+print("writing to file: " + output_file)
 
-def runGen(strategy, steps=200, init=0.50):
+def runGen(strategy, steps=STEPS, init=CANS):
+    rw.graphicsOff()
     if type(strategy) is not str or len(strategy) != 243:
         raise Exception("strategy is not a string of length 243")
     for char in strategy:
@@ -56,38 +64,78 @@ def runGen(strategy, steps=200, init=0.50):
 
 
 # loop over #generations 
-for g in xrange(generation_count): 
+for g in xrange(GENERATIONS): 
     results = []
+
     # loop over population
     for strategy in population: 
         results.append((runGen(strategy), strategy))
-        # -- modified demo() -- Shepard
-        # create reward counter
-        # loop over #actions
-            # look to get percept
-            # perform action and update reward
 
-    # log and display results
-    results.sort()
+    # sort the results
+    results.sort(key=lambda x: x[0], reverse=True)
+    # print(list(map(lambda x: x[0], results[:10])))
+
+    # get metrics on the population
+    averageFitness = sum(map(lambda x: x[0], results)) / len(results)
+    bestFitness = results[0][0]
+    bestStrategy = results[0][1]
+    # print("best fitness: " + str(bestFitness))
     
-    averageFitness = sum(map(lambda x: x[0], results))
-    bestFitness = results[-1][0]
-    bestStrategy = results[-1][1]
+    # get the sorted population from the results
+    population = list(map(lambda x: x[1], results))
 
     # every 10 generations, log the best performer
-    if (g % 10 == 0 or g == generation_count - 1): 
+    if (g % 10 == 0 or g == GENERATIONS - 1): 
         with open(output_file, 'a') as file:
             file.write("{0} {1} {2} {3}\n".format(g, averageFitness, bestFitness, bestStrategy))
 
     # every 20 generations, demo the best performer
-    if (g % 20 == 0 or g == generation_count - 1):
-        rw.demo(bestStrategy)
+    if (g % 20 == 0 or g == GENERATIONS - 1):
+        rw.demo(bestStrategy, steps=STEPS, init=CANS)
 
     # select next generation / mutate / crossover -- Jack
-    # newPopulation = []
-    # for p in range(len(population)):
-    #     # get parents
+    # take the top 4 from previous generation
+    newPopulation = population[:4]
+    for p in range((len(population) - len(newPopulation)) // 2):
+        # get parents
 
+        p = (np.array(list(reversed(range(len(population)))), dtype='float') + 1) / (len(population) * (len(population) + 1) / 2) 
+        parent1, parent2 = np.random.choice(population, size=2, replace=False, p=p)
+
+        # def randIndi(size):
+        #     randNum = random.randint(0, size * (size - 1) / 2)
+        #     N = math.floor(math.sqrt(2 * randNum))
+        #     if (N * (N + 1) <= 2 * randNum and 2 * randNum < (N + 1) * (N + 2)):
+        #         return N - 1
+        #     else :
+        #         return N
+
+        # N = len(population)
+        # parent1 = population[N - 1 - int(randIndi(N))]
+        # parent2 = population[N - 1 - int(randIndi(N))]
+
+        # print('parent1 index:',population.index(parent1))
+        # print('parent2 index:',population.index(parent2))
+
+        # cross the parents' Genome
+        cross_point = random.randint(0, len(parent1)-1)
+        child1 = parent1[:cross_point] + parent2[cross_point:]
+        child2 = parent2[:cross_point] + parent1[cross_point:]
+
+        # randomly mutate
+        for i in range(len(child1)):
+            if (random.random() > 0.990):
+                child1 = child1[:i] + str(random.choice(list(xrange(7)))) + child1[i+1:]
+            if (random.random() > 0.990):
+                child2 = child2[:i] + str(random.choice(list(xrange(7)))) + child2[i+1:]
+
+        assert(len(child1) == 243)
+        assert(len(child2) == 243)
+        newPopulation.append(child1)
+        newPopulation.append(child2)
+
+    population = newPopulation
+    assert(len(population) == POP_SIZE)
 
 
 
