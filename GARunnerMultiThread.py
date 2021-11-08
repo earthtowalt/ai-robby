40ded4e885d8ed6e644db47ee67882f32b082dd6
shepardxia <weitao.xia@vanderbilt.edu>
2021-11-07 19:12:37 -0600

    multithreading

---
+++ GARunnerMultiThread.py
@@ -0,0 +1,207 @@
+import random
+import time
+import math
+import threading
+
+import robby 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+rw = []
+for i in xrange(4):
+    rw.append(robby.World(10,10))
+# rw.demo(rw.strategyM)
+
+
+# create robby world
+
+# instantiate GA population (list of strategy strings) -- Jack 
+# - size 200
+# - crossover 1.0 (100% single point)
+# - mutation: 0.005 per locus
+# - actions per step: 200
+# - generations: 500
+# - total reward summed over 100 sessions
+
+def generateRandomStrategy():
+    return ''.join([str(random.choice(list(xrange(7)))) for i in xrange(243)])
+
+POP_SIZE = 200
+#population = [generateRandomStrategy() for i in xrange(POP_SIZE)]
+
+GENERATIONS = 1000
+STEPS = 200
+CANS = 0.5
+
+
+population = []
+for i in xrange(4):
+    population.append([generateRandomStrategy() for i in xrange(POP_SIZE)])
+
+
+def runGen(threadNum, strategy, steps=STEPS, init=CANS):
+    rw[threadNum].graphicsOff()
+    if type(strategy) is not str or len(strategy) != 243:
+        raise Exception("strategy is not a string of length 243")
+    for char in strategy:
+        if char not in "0123456":
+            raise Exception("strategy contains a bad character: '%s'" % char)
+    if type(steps) is not int or steps < 1:
+        raise Exception("steps must be an integer > 0")
+
+    if type(init) is str:
+        # init is a config file
+        rw[threadNum].load(init)
+    elif type(init) in [int, float] and 0 <= init <= 1:
+        # init is a can density
+        rw[threadNum].goto(0, 0)
+        rw[threadNum].distributeCans(init)
+    else:
+        raise Exception("invalid initial configuration")
+    history = []
+    cycleDetected = False
+    cycleStart = 0
+    reward = 0
+    for i in xrange(steps):
+        p = rw[threadNum].getPerceptCode()
+        action = robby.POSSIBLE_ACTIONS[int(strategy[p])]
+        reward += rw[threadNum].performAction(action)
+    return reward
+
+
+def simulateGeneration(mutationRate, threadNum):
+
+    output_file = 'GAoutput_{0}.txt'.format(mutationRate)
+    print("writing to file: " + output_file)
+
+    # loop over #generations 
+    for g in xrange(GENERATIONS): 
+        results = []
+    # generate set of random maps and save them
+    #for i in range(5):
+    #    rw.distributeCans(CANS)
+    #    rw.goto(0,0)
+    #    rw.save('config_{0}.txt'.format(i))
+
+    # loop over population, testing each map
+    #for strategy in population: 
+    #    total_score = sum([runGen(strategy, steps=STEPS, init='config_{0}.txt'.format(i)) for i in range(5)])
+    #    results.append((total_score / 5, strategy))
+
+        for strategy in population[threadNum]:
+            results.append((runGen(threadNum, strategy, steps=STEPS, init=CANS), strategy))
+        # sort the results
+        results.sort(key=lambda x: x[0], reverse=True)
+        # print(list(map(lambda x: x[0], results[:10])))
+
+        # get metrics on the population
+        averageFitness = sum(map(lambda x: x[0], results)) / len(results)
+        bestFitness = results[0][0]
+        bestStrategy = results[0][1]
+        # print("best fitness: " + str(bestFitness))
+        
+        # get the sorted population from the results
+        population[threadNum] = list(map(lambda x: x[1], results))
+
+        # every 10 generations, log the best performer
+        if (g % 10 == 0 or g == GENERATIONS - 1): 
+            with open(output_file, 'a') as file:
+                file.write("{0} {1} {2} {3}\n".format(g, averageFitness, bestFitness, bestStrategy))
+
+        # every 50 generations, demo the best performer
+        #    rw.demo(bestStrategy, steps=STEPS, init='config_0.txt')
+
+        # select next generation / mutate / crossover -- Jack
+        # take the top 4 from previous generation
+
+        indicies = []
+        newPopulation = population[threadNum][:10]
+        for p in range((len(population[threadNum]) - len(newPopulation)) // 2):
+            # get parents
+
+            # linear distribution
+            # raw_indicies = np.array(list(reversed(range(len(population)))), dtype='float') + 1
+
+            # exponential distribution
+            # raw_indicies = np.exp((500 - np.arange(0, len(population), 1, dtype='float')) / 90) - 25
+
+            # quadratic distribution
+            raw_indicies = np.array(list(reversed(range(len(population[threadNum])))), dtype='float') ** 2
+
+            p = raw_indicies / np.sum(raw_indicies) 
+            parent1, parent2 = np.random.choice(population[threadNum], size=2, replace=False, p=p)
+
+            indicies.append(population[threadNum].index(parent1))
+            indicies.append(population[threadNum].index(parent2))
+
+            # def randIndi(size):
+            #     randNum = random.randint(0, size * (size - 1) / 2)
+            #     N = math.floor(math.sqrt(2 * randNum))
+            #     if (N * (N + 1) <= 2 * randNum and 2 * randNum < (N + 1) * (N + 2)):
+            #         return N - 1
+            #     else :
+            #         return N
+
+            # N = len(population)
+            # parent1 = population[N - 1 - int(randIndi(N))]
+            # parent2 = population[N - 1 - int(randIndi(N))]
+
+            # print('parent1 index:',population.index(parent1))
+            # print('parent2 index:',population.index(parent2))
+
+            # cross the parents' Genome
+            cross_point = random.randint(0, len(parent1)-1)
+            child1 = parent1[:cross_point] + parent2[cross_point:]
+            child2 = parent2[:cross_point] + parent1[cross_point:]
+
+            # randomly mutate
+            for i in range(len(child1)):
+                if (random.random() > mutationRate):
+                    child1 = child1[:i] + str(random.choice(list(xrange(7)))) + child1[i+1:]
+                if (random.random() > mutationRate):
+                    child2 = child2[:i] + str(random.choice(list(xrange(7)))) + child2[i+1:]
+
+            assert(len(child1) == 243)
+            assert(len(child2) == 243)
+            newPopulation.append(child1)
+            newPopulation.append(child2)
+            
+        population[threadNum] = newPopulation
+        assert(len(population[threadNum]) == POP_SIZE)
+    
+
+
+
+print ("aha")
+
+thread1 = threading.Thread(target=simulateGeneration, args=(0.980, 0,))
+thread2 = threading.Thread(target=simulateGeneration, args=(0.985, 1,))
+thread3 = threading.Thread(target=simulateGeneration, args=(0.990, 2,))
+thread4 = threading.Thread(target=simulateGeneration, args=(0.995, 3,))
+
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+print ("aha")
+
+thread1.join()
+thread2.join()
+thread3.join()
+thread4.join()   
+
+    
+
+    # show index distribution for parent selection
+    # print('making histogram')
+    # print('num indicies:', len(indicies))
+    # plt.hist(indicies, density=True, bins=20)
+    # plt.show()
+    # print('made hist')
+    # raw_input()
+
+
+
+
